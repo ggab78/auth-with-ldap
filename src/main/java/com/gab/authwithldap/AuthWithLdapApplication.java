4f70e1ea6c0806ef47dfa7287fdc0c93a91b4aa5
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-07-29 08:46:24 +0200

    initial project

---
+++ src/main/java/com/gab/authwithldap/AuthWithLdapApplication.java
@@ -0,0 +1,13 @@
+package com.gab.authwithldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWithLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthWithLdapApplication.class, args);
+    }
+
+}
